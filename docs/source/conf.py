fea3b2265b09311e382b05bd2776330590a2f962
Ivan Morozov <i.a.morozov@inp.nsk.su>
2023-05-15 22:12:33 +0700

    15_05_2023: fix docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,17 @@
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+import os
+import sys
+from os.path import dirname
+
+SOURCE = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, SOURCE)
+
+ROOT = dirname(dirname(SOURCE))
+sys.path.insert(0, ROOT)
+
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
